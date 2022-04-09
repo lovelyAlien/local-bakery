5003809518605911b20931e0d7a6a5f1944f23c3
lovelyAlien <jaeseung425@gmail.com>
2022-04-10 02:09:25 +0900

    feat: S3 이미지 업로드 기능 추가

--- bakery-api/src/main/java/com/localbakery/LocalBakeryApplication.java
+++ bakery-api/src/main/java/com/localbakery/LocalBakeryApplication.java
@@ -3,6 +3,7 @@ package com.localbakery;
 import com.localbakery.authentication.oauth2.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -15,8 +16,19 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 public class LocalBakeryApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.properties,"
+            + "classpath:aws.yml";
+
+
+
     public static void main(String[] args) {
-        SpringApplication.run(LocalBakeryApplication.class, args);
+//        SpringApplication.run(LocalBakeryApplication.class, args);
+
+
+        new SpringApplicationBuilder(LocalBakeryApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
     @Bean
