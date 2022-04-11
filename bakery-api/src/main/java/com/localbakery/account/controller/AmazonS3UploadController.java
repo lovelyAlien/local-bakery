038f8c0aa05cf72404c408a1c46edb51fbc39a1b
jinwoo-Bae <wlsdn3578@gmail.com>
2022-04-11 22:25:21 +0900

    Fix: use security context for identifying user

--- bakery-api/src/main/java/com/localbakery/account/controller/AmazonS3UploadController.java
+++ bakery-api/src/main/java/com/localbakery/account/controller/AmazonS3UploadController.java
@@ -3,8 +3,10 @@ package com.localbakery.account.controller;
 import com.localbakery.account.service.S3UploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,8 +20,8 @@ public class AmazonS3UploadController {
     private final S3UploadService s3UploadService;
 
     @PostMapping(value = "/upload")
-    public ResponseEntity<Map<String, String>> uploadFile(String userEmail, MultipartFile multipartfile) {
+    public ResponseEntity<Map<String, String>> uploadFile(@RequestParam(value="image") MultipartFile multipartfile) {
 
-        return ResponseEntity.ok(s3UploadService.updateProfileImage(userEmail, multipartfile));
+        return ResponseEntity.ok(s3UploadService.updateProfileImage(SecurityContextHolder.getContext().getAuthentication().getName(), multipartfile));
     }
 }
