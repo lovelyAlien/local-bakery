5665d9015aba194925419f4ffd3162016e0d0b57
lovelyAlien <jaeseung425@gmail.com>
2022-04-24 10:36:49 +0900

    fix: 중복되는 S3 빈 삭제

--- bakery-api/src/main/resources/sql/schema.sql
+++ bakery-api/src/main/resources/sql/schema.sql
@@ -32,7 +32,8 @@ CREATE TABLE iF NOT EXISTS localbakery.accounts
     accountId INT AUTO_INCREMENT PRIMARY KEY,
     email     VARCHAR(255),
     userName  VARCHAR(255),
-    imageUrl  VARCHAR(255)
+    imageUrl  VARCHAR(255),
+    hometownId INT
 ) ENGINE = INNODB;
 
 DROP TABLE IF EXISTS localbakery.hometown;
